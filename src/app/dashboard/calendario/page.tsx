5571fd1738cc9260ef3c099715468553bab3ad2f
Guilherme Vianna <mataveli91@gmail.com>
2025-07-04 09:52:14 -0300

    fix: ajustando rotas de delete

--- src/app/dashboard/calendario/page.tsx
+++ src/app/dashboard/calendario/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { apiGet } from "@/app/utils/api";
+import { apiDelete, apiGet } from "@/app/utils/api";
 import { API_ROUTES } from "@/app/utils/routes";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -60,7 +60,8 @@ export default function calendarioPage() {
     }
   };
 
-  const handleDelete = (id: any) => {
+  const handleDelete = async (id: any) => {
+    await apiDelete<any>(`${API_ROUTES.CALENDARIO}?${id.toString()}`);
     setData((prev) => prev.filter((item) => item.id !== id));
   };
 
